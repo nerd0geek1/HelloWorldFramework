735e13361a9168a70071023ab8df19a10527deea
Kohei Tabata <nerd0geek1@gmail.com>
2016-03-20 19:46:07 +0900

    add Hello class

---
+++ HelloWorld/Hello.swift
@@ -0,0 +1,16 @@
+//
+//  Hello.swift
+//  HelloWorld
+//
+//  Created by Kohei Tabata on 2016/03/20.
+//
+//
+
+import UIKit
+
+class Hello: NSObject {
+
+    class func world() {
+        print("Hello World")
+    }
+}
