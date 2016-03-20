72d72ab9c13b693d83c78ef2b57069f66967790b
Kohei Tabata <nerd0geek1@gmail.com>
2016-03-20 19:35:08 +0900

    Initial commit

---
+++ HelloWorld/HelloWorld.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorld.h
+//  HelloWorld
+//
+//  Created by Kohei Tabata on 2016/03/20.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloWorld.
+FOUNDATION_EXPORT double HelloWorldVersionNumber;
+
+//! Project version string for HelloWorld.
+FOUNDATION_EXPORT const unsigned char HelloWorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorld/PublicHeader.h>
+
+
